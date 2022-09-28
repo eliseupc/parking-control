58b612140ff5f25fb98ebf86a5d44bc616821903
Eliseu Correa <eliseupaulinocorrea@hotmail.com>
2022-09-27 22:23:57 -0300

    API - Controle de Estacionamento

---
+++ src/main/java/br/com/eliseucorrea/parkingcontrol/repositories/ParkingSpotRepository.java
@@ -0,0 +1,15 @@
+package br.com.eliseucorrea.parkingcontrol.repositories;
+
+import br.com.eliseucorrea.parkingcontrol.models.ParkingSpotModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface ParkingSpotRepository extends JpaRepository<ParkingSpotModel, UUID> {
+    boolean existsByLicensePlateCar(String licencePlateCar);
+    boolean existsByParkingSpotNumber(String parkingSpotNumber);
+    boolean existsByApartmentAndBlock(String apartment, String block);
+
+}
